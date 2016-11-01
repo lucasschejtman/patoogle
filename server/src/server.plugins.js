d762a8f0659806549be0258c50f8bb02750b3650
Lucas <lucas.schejtman@gmail.com>
2016-11-01 16:24:54 +1300

    tidy up hapi plugins

--- server/src/server.plugins.js
+++ server/src/server.plugins.js
@@ -1,24 +1,22 @@
-const plugins = [
-  require('inert'),
-  require('vision'),
-  require('hapi-auth-jwt2'),
-  {
-    register: require('hapi-swagger'),
-    options: {
-      info: {
-        title: 'Pathoo API Documentation'
-      },
-      schemes: ['http', 'https'],
-      tags: [{ name: 'auth' }]
-    }
+const swaggerOpts = {
+  info: {
+    title: 'Pathoo API Documentation'
   },
-  {
-    register: require('hapi-router'),
-    options: {
-      cwd: __dirname,
-      routes: './modules/**/*.routes.js'
-    }
-  }
+  schemes: ['http', 'https'],
+  tags: [{ name: 'auth' }]
+};
+
+const routerOpts = {
+  cwd: __dirname,
+  routes: './modules/**/*.routes.js'
+};
+
+const plugins = [
+  { register: require('inert') },
+  { register: require('vision') },
+  { register: require('hapi-auth-jwt2') },
+  { register: require('hapi-swagger'), options: swaggerOpts },
+  { register: require('hapi-router'), options: routerOpts }
 ];
 
 export default plugins;
