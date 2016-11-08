7f8feab4e38583ea8b03ac6d4d376e14b0d6edfa
Lucas <lucas.schejtman@gmail.com>
2016-11-08 16:13:32 +1300

    changed to synchronous joi validation

--- server/test/modules/patent/patent.schema.test.js
+++ server/test/modules/patent/patent.schema.test.js
@@ -5,7 +5,8 @@ import Joi from 'joi';
 
 test('getSchema request schema is valid', t => {
   const request = { id: "6ff08836-0c5f-471c-9656-440c98c8b1b7" };
-  Joi.validate(request, target.get.request, (err, value) => {
-    t.is(err, null);
-  });
+  const result = Joi.validate(request, target.get.request);
+
+  t.is(result.error, null);
+  t.deepEqual(result.value, request);
 });
