93eb0f8a57cb1903a37592770d165a66a40fadd2
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-11-27 11:27:30 +1300

    cli re structure

--- importer/src/cli.js
+++ importer/src/cli.js
@@ -2,15 +2,23 @@ import pkg from '../package';
 
 import program from 'commander';
 
-program
-  .version(pkg.version)
-  .option('-d, --dry', 'Performs an import without side effects');
+//TODO: Move elsewhere
+const doImport = (year, month, day, dry) => {
+  console.log(year);
+  console.log(month);
+  console.log(day);
+  console.log(dry);
+};
 
 program
-  .command('import [year] [week]')
-  .action((year, week) => {
-    console.log(year);
-    console.log(week);
-  });
+  .version(pkg.version)
+  .option('--dry', 'Performs an import without side effects')
+  .option('-y, --year <year>', 'Year of the bulk to import')
+  .option('-m, --month <month>', 'Month of the bulk to import')
+  .option('-d, --day <day>', 'Day of the bulk to import');
+
+program
+  .command('import')
+  .action(() => doImport(program.year, program.month, program.day, program.dry));
 
 program.parse(process.argv);
