595c86d7f7b779dfab6afbee034dac7008c6633b
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-11-28 07:41:29 +1300

    added local flag

--- importer/src/cli.js
+++ importer/src/cli.js
@@ -3,7 +3,7 @@ import pkg from '../package';
 import program from 'commander';
 
 //TODO: Move elsewhere
-const doImport = (year, month, day, dry) => {
+const doImport = ({ year, month, day, dry }) => {
   console.log(year);
   console.log(month);
   console.log(day);
@@ -12,13 +12,14 @@ const doImport = (year, month, day, dry) => {
 
 program
   .version(pkg.version)
-  .option('--dry', 'Performs an import without side effects')
   .option('-y, --year <year>', 'Year of the bulk to import')
   .option('-m, --month <month>', 'Month of the bulk to import')
-  .option('-d, --day <day>', 'Day of the bulk to import');
+  .option('-d, --day <day>', 'Day of the bulk to import')
+  .option('--dry', 'Performs an import without side effects')
+  .option('--local', 'Determines whether it should download the bulk file or fetch it from the file system');
 
 program
   .command('import')
-  .action(() => doImport(program.year, program.month, program.day, program.dry));
+  .action(() => doImport(program));
 
 program.parse(process.argv);
