d6b671671cc602164f269d8662127e01c5dd4923
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-12-03 13:58:39 +1300

    playing with dif xml libraries

--- importer/src/xml.js
+++ importer/src/xml.js
@@ -1,22 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import xmlStream from 'xml-stream';
-
-const elName = 'us-bibliographic-data-grant';
-
-const pauser = xml => {
-  xml.pause();
-  setTimeout(() => xml.resume(), 1000);
-};
+import libxml from 'libxmljs';
 
 export const run = () => {
-  const stream = fs.createReadStream(path.join(__dirname, 'ipg160105.xml'));
-  const xml = new xmlStream(stream);
-
-  xml.collect(elName);
-  xml.on(`endElement: ${elName}`, (element) => {
-      console.log(element);
-      pauser(xml);
+  const buffer = fs.readFile(path.join(__dirname, 'one_patent.xml'),{ encoding: 'utf8' }, (err, data) => {
+    const xmlDoc = libxml.parseXmlString(data);
+    console.log(xmlDoc.root());
   });
-  xml.on('error', function(){});
 };
