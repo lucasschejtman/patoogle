280b0dc0c69660a52898b448c18132ca4c8d4187
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-03-12 11:34:45 +1300

    playing with diff ux libraries

--- client/components/layout.js
+++ client/components/layout.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import { Page } from 'hedron';
+//import { Page } from 'hedron';
+import App from 'grommet/components/App';
+import Box from 'grommet/components/Box';
+import Header from 'grommet/components/Header';
+import Title from 'grommet/components/Title';
 
 export default ({ children, title = 'This is the default title' }) => (
   <div>
@@ -9,17 +13,43 @@ export default ({ children, title = 'This is the default title' }) => (
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
-    <header>
+    {/* <header>
       <nav>
         <Link href='/'><a>Home</a></Link> |
         <Link href='/about'><a>About</a></Link> |
         <Link href='/contact'><a>Contact</a></Link>
       </nav>
-    </header>
+    </header> */}
 
-    <Page>
+    <App>
+      <Header splash={false}
+        size='small'
+        float={false}
+        fixed={true}>
+        <Title>
+          Sample Title
+        </Title>
+        <Box flex={true}
+          justify='end'
+          direction='row'
+          responsive={false}>
+          {/* <Menu icon={<Actions />}
+            dropAlign={{"right": "right", "top": "top"}}>
+            <Anchor href='#'
+              className='active'>
+              First
+            </Anchor>
+            <Anchor href='#'>
+              Second
+            </Anchor>
+            <Anchor href='#'>
+              Third
+            </Anchor>
+          </Menu> */}
+        </Box>
+      </Header>
       { children }
-    </Page>
+    </App>
 
     <footer>
       I`m here to stay
