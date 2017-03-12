755015c9593e766bfd4ee093c67ea6400812cd8f
Lucas <lucas.schejtman@gmail.com>
2017-03-13 11:41:52 +1300

    make next work with grommet

--- client/components/layout.js
+++ client/components/layout.js
@@ -7,52 +7,29 @@ import Header from 'grommet/components/Header';
 import Title from 'grommet/components/Title';
 
 export default ({ children, title = 'This is the default title' }) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    {/* <header>
-      <nav>
-        <Link href='/'><a>Home</a></Link> |
-        <Link href='/about'><a>About</a></Link> |
-        <Link href='/contact'><a>Contact</a></Link>
-      </nav>
-    </header> */}
+    <div>
+        <Head>
+          <title>{ title }</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <link href="//cdnjs.cloudflare.com/ajax/libs/grommet/1.0.1/grommet.min.css" rel="stylesheet" type="text/css" />
+        </Head>
 
-    <App>
-      <Header splash={false}
-        size='small'
-        float={false}
-        fixed={true}>
-        <Title>
-          Sample Title
-        </Title>
-        <Box flex={true}
-          justify='end'
-          direction='row'
-          responsive={false}>
-          {/* <Menu icon={<Actions />}
-            dropAlign={{"right": "right", "top": "top"}}>
-            <Anchor href='#'
-              className='active'>
-              First
-            </Anchor>
-            <Anchor href='#'>
-              Second
-            </Anchor>
-            <Anchor href='#'>
-              Third
-            </Anchor>
-          </Menu> */}
-        </Box>
-      </Header>
-      { children }
-    </App>
-
-    <footer>
-      I`m here to stay
-    </footer>
-  </div>
+        <App>
+          <Header splash={false}
+            size='small'
+            float={false}
+            fixed={true}>
+            <Title>
+              Sample Title
+            </Title>
+            <Box flex={true}
+              justify='end'
+              direction='row'
+              responsive={false}>
+            </Box>
+          </Header>
+          { children }
+        </App>
+    </div>
 );
